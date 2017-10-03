4c8802000a7af177c25f8d032d0a479ac187dd31
Adderall <30782192+Adderal1@users.noreply.github.com>
2017-10-03 23:32:29 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,17 @@
 import json
-
+import pprint
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='Latin-1') as row_data:
+        data = row_data.read()
+    row_data.close()
+    return json.load(data)
 
 
 def pretty_print_json(data):
-    pass
+    pprint.pprint(data, depth=10, width=10)
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data(input('Enter the path to the file: '))
+    pretty_print_json(data)
